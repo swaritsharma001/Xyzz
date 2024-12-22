8762b6888dc55c38556d2a1700af802b20b4b22f
Swarit <152766614+swaritsharma001@users.noreply.github.com>
2024-12-22 07:26:17 +0530

    Create user.js

---
+++ routes/user.js
@@ -0,0 +1,46 @@
+import express from "express";
+const router = express.Router();
+import User from "../models/user.js";
+import passport from "../passport.js";
+import {verifyToken, verifyAdmin} from "../jwt/jwt.js";
+
+router.get("/google/login", passport.authenticate("google", {
+  scope: ["profile", "email"]
+}));
+
+// Google callback
+router.get("/google/callback", passport.authenticate("google", {
+  failureRedirect: "https://techinwords.tech"
+}), (req, res) => {
+  // Assuming req.user.token exists after successful login
+  res.cookie("token", req.user.token, {
+    maxAge:  2 * 60 * 60 * 60 * 1000,// 15 minutes
+    httpOnly: true, // Ensure cookie is sent only through HTTP(S)
+    secure: process.env.NODE_ENV === 'production', // Only secure in production
+    sameSite: 'None',
+  });
+  res.redirect("https://techinwords.tech");
+});
+
+// Logout
+router.get("/logout", verifyToken, (req, res) => {
+  res.clearCookie("token");
+  res.redirect("https://techinwords.tech");
+});
+
+// Get user data
+router.get("/user", verifyToken, (req, res) => {
+  
+  res.json(req.user);
+});
+
+router.get("/admin", verifyAdmin, (req, res) => {
+  res.json(req.user)
+})
+
+router.get("/admin/users", verifyAdmin, async(req, res) => {
+  const allUsers = await User.find()
+  res.status(200).json(allUsers)
+})
+
+export default router;
